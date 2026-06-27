009b3e7ff2480372ea18e4db5f89755de3d820e0
henriquekom-png <henriquekom@gmail.com>
2026-06-27 00:00:23 -0400

    Create restore point before Cloud Run migration

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-970124e6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4h55miehgm"
+    "revision": "0.n2kgf6a0e5"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
